9b261d3c70463b2e208d709926aa05a5eb6a5fd8
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2020-09-18 01:02:40 +0600

    added two new plugins and started a new project

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -1,3 +1,35 @@
-$(document).ready(function(){
-    $("#sticker").sticky({topSpacing:0});
+$(document).ready(function () {
+    $("#sticker").sticky({topSpacing: 0});
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        items: 1,
+        navs: true
+    })
 });
+
+
+var scroll = new SmoothScroll('a[href*="#"]');
+
+
+(function () {
+    var $projects = $('.gallery');
+
+    $projects.isotope({
+        itemSelector: 'item1'
+    });
+
+    $('ul.category-list').on('click', 'li', function (e) {
+        e.preventDefault();
+
+        var filter = $(this).attr('data-filter');
+
+        $('ul.category-list > li').removeClass('ac');
+        $(this).addClass('ac');
+
+        $projects.isotope({filter: filter});
+    });
+})(jQuery);
+
+
