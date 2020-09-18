20f56069638a6a22077a78e81328ae9f7bf9b274
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2020-09-18 17:54:32 +0600

    integrated all the required plugins

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         loop:true,
         margin:10,
         items: 1,
-        navs: true
+        nav: true
     })
 });
 
@@ -13,23 +13,23 @@ $(document).ready(function () {
 var scroll = new SmoothScroll('a[href*="#"]');
 
 
-(function () {
-    var $projects = $('.gallery');
-
-    $projects.isotope({
-        itemSelector: 'item1'
-    });
-
-    $('ul.category-list').on('click', 'li', function (e) {
-        e.preventDefault();
-
-        var filter = $(this).attr('data-filter');
-
-        $('ul.category-list > li').removeClass('ac');
-        $(this).addClass('ac');
-
-        $projects.isotope({filter: filter});
-    });
-})(jQuery);
+// (function () {
+//     var $projects = $('.gallery');
+//
+//     $projects.isotope({
+//         itemSelector: 'item1'
+//     });
+//
+//     $('ul.category-list').on('click', 'li', function (e) {
+//         e.preventDefault();
+//
+//         var filter = $(this).attr('data-filter');
+//
+//         $('ul.category-list > li').removeClass('ac');
+//         $(this).addClass('ac');
+//
+//         $projects.isotope({filter: filter});
+//     });
+// })(jQuery);
 
 
