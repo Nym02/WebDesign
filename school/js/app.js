1c5bac97fdc2dc88659028d54afe88b288d26666
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2020-12-07 00:06:35 +0600

    completed the OUR TEACHERS section

--- school/js/app.js
+++ school/js/app.js
@@ -8,7 +8,6 @@ function showSun(index){
         node.classList.remove('class-day-active');
     })
     btn[index].classList.add('class-day-active');
-    btn[index].style.outline = 'none';
 
     content.forEach((node)=>{
         node.style.display='none';
